0cc695233da43d98358061d9e309706595c1ad16
Kanhaiya1232 <101667615+Kanhaiya1232@users.noreply.github.com>
2022-05-15 13:24:01 +0530

    Update CONDITON.C

--- CONDITON.C
+++ CONDITON.C
@@ -7,3 +7,21 @@ clrscr();
 printf("%d",(a>b)?a:b);
 getch();
 }
+
+
+***************************************************Find marriage status******************************
+#include<stdio.h>  
+#include<string.h>  
+int main()  
+{  
+    int age;
+    char status;
+    printf("Enter the age: ");
+    scanf("%d",&age);
+    status = (age>22) ? 'M': 'U';  
+    if(status == 'M')  
+    printf("Married");  
+    else  
+    printf("Unmarried");  
+    return 0;  
+}     
